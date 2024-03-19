49fc59a50b1bfbbc5a24f200ffb73cf154d56013
ajie <2314454208@qq.com>
2024-03-19 13:22:08 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,102 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "../utils/auto_sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: "阿杰的文档项目",
+  description: "A VitePress Site",
+  themeConfig: {
+    outlineTitle:"目录",
+    outline:[2,5],
+    logo:'/logo.png',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {
+        text: '前端', items: [
+          { text: 'HTML', link: "/front-end/" },
+          { text: 'CSS', link: "/" },
+          { text: 'JavaScript', link: "/" },
+          { text: 'vitepress', link: "/" },
+        ]
+      },
+      {
+        text: '编程语言', items: [
+          { text: 'Java', link: "/" },
+          { text: 'Python', link: "/" },
+          { text: 'C语言', link: "/" },
+          { text: 'SQL', link: "/" },
+        ]
+      },
+      {
+        text: '嵌入式',
+        items: [
+          {
+            text: '基础知识', items: [
+              { text: '模电', link: "/" },
+              { text: '数电', link: "/" }
+            ]
+          },
+          {
+            text: '硬件平台', items: [
+              { text: '51单片机', link: "/" },
+              { text: 'stm32', link: "/" }
+            ]
+          },
+          {
+            text: '系统技术', items: [
+              { text: 'Linux', link: "/" },
+              { text: '实时操作系统（RTOS）', link: "/" }
+            ]
+          }
+        ]
+      },
+      {
+        text:"两边栏", link:"/两边栏"
+      },
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    // sidebar:{
+    //   "/front-end": set_sidebar("/front-end")
+    // },
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/AlwayStudent1' }
+    ],
+    footer: {
+      copyright: "Copyright@ 2024 阿杰"
+    },
+  
+     // 设置搜索框的样式
+     search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },  
+  }
+})
+base: "/Demo/"
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "阿杰的文档项目"
+  text: "构建知识库"
+  tagline: 且视他人之疑目如盏盏鬼火，大胆地走你的夜路
+  image:
+    src: /background.png
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: 特征一
+    details: 特征一的详细描述
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
